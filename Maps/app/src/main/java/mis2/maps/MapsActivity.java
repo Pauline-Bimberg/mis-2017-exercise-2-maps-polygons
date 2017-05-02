3e918933c07e11bf55f1c96d4372fe0b9948fa53
Clara Pauline Bimberg <peni1673@in.uni-weimar.de>
2017-05-02 16:55:27 +0200

    setting markers now works

--- Maps/app/src/main/java/mis2/maps/MapsActivity.java
+++ Maps/app/src/main/java/mis2/maps/MapsActivity.java
@@ -1,11 +1,18 @@
 package mis2.maps;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.preference.Preference;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -14,15 +21,27 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
 
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
 import java.util.Set;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private EditText editText;
-    private SharedPreferences sharedPref;
+    private Button startPoly;
+    private Button endPoly;
+    private boolean polyStarted = false;
+    PolygonOptions polygon = new PolygonOptions();
+    private int polycounter = 0;
+
+
+    SharedPreferences sharedPref;
+
     private SharedPreferences.Editor editor;
     private int counter = 0;
 
@@ -39,8 +58,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
 
-
-
     }
 
 
@@ -55,10 +72,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
         mMap = googleMap;
-
-
         editText = (EditText) findViewById(R.id.editText);
+        startPoly = (Button) findViewById(R.id.startPoly);
+        endPoly = (Button) findViewById(R.id.endPoly);
+        Polygon mapPolygon = mMap.addPolygon(polygon);
+
+        // TODO: Code inspired by:
+        sharedPref = getSharedPreferences("Locations", Context.MODE_PRIVATE);
+        counter = sharedPref.getInt("Counter",0);
+
+        if(counter != 0){
+            String latitude = "";
+            String longitude = "";
+            String text = "";
+
+            for(int i = 0; i<counter; i++){
+                String s = Integer.toString(i);
+                latitude = sharedPref.getString("lat"+s, "0");
+                longitude = sharedPref.getString("lng" +s,"0");
+                text = sharedPref.getString("text"+s, "0");
+
+                LatLng position = new LatLng(Double.parseDouble(latitude), Double.parseDouble(longitude));
+
+                mMap.addMarker(new MarkerOptions().position(position).title(text));
+            }
+        }
+
 
         // Move view to Weimar
         LatLng weimar = new LatLng(50.98, 11.33);
@@ -70,28 +111,53 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             @Override
             public void onMapLongClick(LatLng latLng) {
+
                 String text = editText.getText().toString();
                 String counterString = Integer.toString(counter);
+                sharedPref = getSharedPreferences("Locations", Context.MODE_PRIVATE);
+                counter++;
 
                 editor = sharedPref.edit();
-                editor.clear();
-                Set<String> oldSet = sharedPref.getStringSet("key", new HashSet<String>());
 
-
-                Set<String> newSet = new HashSet<String>();
-                newSet.add(counterString+"lat"+Double.toString(latLng.latitude));
-                newSet.add(counterString+"long"+Double.toString(latLng.longitude));
-                newSet.add(counterString+"text"+ text);
-                newSet.addAll(oldSet);
-
-                editor.putStringSet("key", newSet);
+                editor.putString("lat" + counterString , Double.toString(latLng.latitude));
+                editor.putString("lng" + counterString , Double.toString(latLng.longitude));
+                editor.putString("text" + counterString , text);
+                editor.putInt("Counter", counter);
                 editor.commit();
-                counter++;
+
+
 
                 mMap.addMarker(new MarkerOptions().position(latLng).title(text));
 
+                if(polyStarted = true){
+
+                    polygon
+                            .add( latLng);
+
+                }
+
             }
         });
+
+        startPoly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                polyStarted = true;
+
+            }
+        });
+
+        endPoly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                polyStarted = false;
+                // TODO: Berechnung Shape, Setzen Mittelpunkt mit beschriftung
+
+
+
+            }
+        });
+
     }
 
 
